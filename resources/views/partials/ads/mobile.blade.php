bd211367619b5bc562f922a670af4698fd42093c
Marc Tallo <thebakerdev@gmail.com>
2020-07-08 21:49:05 +0800

    updated ad links

--- resources/views/partials/ads/mobile.blade.php
+++ resources/views/partials/ads/mobile.blade.php
@@ -1,5 +1,5 @@
 <div class="ad ad--mobile mx-auto mb-8 md:hidden">
-    <a href="https://itadakiru.com/" target="_blank">
+    <a href="https://connollysekai.com/" target="_blank">
         <img src="{{ $image }}" alt="Ad Image">
     </a>
 </div>
